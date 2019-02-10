07253aa2839e7d3ce2d1231bdae2dea15b7dd231
長谷川有那 <hasegawayuuna@yuna.local>
2019-02-10 17:09:30 +0900

    連想配列の勉強

--- php/html/index.php
+++ php/html/index.php
@@ -1,6 +1,17 @@
 <?php
-$p = "PHP";
-echo "土日でわかる".$p."プログラミング教室!!!!!";
+$str="曜日でわかるPHPプログラミング教室";
+$array_week=array("Sun"=>"日","Mon"=>"月","Tue"=>"火","Wed"=>"水","Thr"=>"木","Fei"=>"金","Sat"=>"土");
+echo $array_week ["Sun"].$str;
 echo "<br>";
-echo "donichi".$p;
-?>
+echo $array_week ["Mon"].$str;
+echo "<br>";
+echo $array_week ["Tue"].$str;
+echo "<br>";
+echo $array_week ["Wed"].$str;
+echo "<br>";
+echo $array_week ["Thr"].$str;
+echo "<br>";
+echo $array_week ["Fei"].$str;
+echo "<br>";
+echo $array_week ["Sat"].$str;
+echo "<br>";
